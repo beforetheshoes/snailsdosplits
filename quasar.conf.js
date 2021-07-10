4aed71e6857dd1f9830cfcf5cbe71805cd3f5698
Ryan Williams <oneryanleewilliams@gmail.com>
2021-07-10 13:17:39 -0400

    ggsgdf

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,7 +47,11 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      env: require('dotenv').config().parsed,
+      //env: require('dotenv').config().parsed,
+      env: {
+        VUE_APP_CLIENT_ID: process.env.VUE_APP_CLIENT_ID,
+        VUE_APP_REDIRECT_URI: process.env.VUE_APP_REDIRECT_URI
+      },
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
